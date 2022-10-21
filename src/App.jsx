bef24cd76dd38ce6653ee7a75464cfa023415e98
Albert Hovhannisyan <albert.hovhannisyan.work@gmail.com>
2022-10-21 19:18:29 +0400

    draft

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Employees from "./components/Employees";
+import Jobs from "./components/Jobs";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Employees/>
+      <Jobs/>
+    </div>
+  );
+}
+
+export default App;
